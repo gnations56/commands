29282d5ad75eee774added28c08a38647efa939d
gnations56 <gnations56@gmail.com>
2017-04-08 10:20:09 -0700

    initial commit of network everywhere support library

---
+++ commands/commands/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+
+namespace commands
+{
+    public static class commands
+    {
+        public static partial class servercom
+        {
+
+
+        }
+
+        public static partial class clientcom
+        {
+
+
+        }
+
+
+   }
+
+    
+}
+
+namespace packages
+{
+    public static class packages
+    {
+
+        public static string[] loadFile(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+
+            return file;
+
+        }
+
+        public static string getFileName(string path)
+        {
+            string filename = Path.GetFileName(path);
+            return filename;
+
+
+        }
+        public static string getTimestamp()
+        {
+            string ts = DateTime.Now.ToString();
+            return ts;
+
+        }
+        public static List<List<string>> registerPackage(string timestamp, string filename, List<List<string>> registeredPackages)
+        {
+            List<string> package = new List<string> { timestamp, filename };
+            registeredPackages.Add(package);
+            Console.WriteLine("Package added successfully");
+            return registeredPackages;
+        }
+
+        public static List<List<string>> deregisterPackage(string timestamp, string filename, List<List<string>> registeredPackages)
+        {
+            List<string> package = new List<string> { timestamp, filename };
+
+            registeredPackages.Remove(package);
+            Console.WriteLine("Package removed successfully");
+            return registeredPackages;
+        }
+
+       
+            }
+        }
+
+
+namespace connections
+{
+
+}
